0c2b3b019eb9deadeef1a3eeedc8d4a4c071f31b
monuyadav6284 <monu628493@gmail.com>
2023-12-23 17:27:49 +0530

    using MERN stact creat Login page and Registration page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes
+
 import './App.css';
+import Homepage from './Components/Homepage/Homepage';
+import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
 
 function App() {
+  const [user, setLoginUser] = useState({});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes> 
+          <Route path="/" element={user && user._id ? <Homepage setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser} />} />
+          <Route path="/login" element={<Login setLoginUser={setLoginUser} />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+
+
